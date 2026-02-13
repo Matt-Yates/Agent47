bb481c48b3d0da01f59629ed86e6f595e7a370d6
Matt-Yates <mattyatesecu@gmail.com>
2026-02-12 20:14:17 -0500

    Updated to intake command line arguments as prompt

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
-
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key = api_key)
-response = client.models.generate_content(
-    model='gemini-2.5-flash', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
+import argparse
+from google.genai import types
 
 def main():
-    print("Hello from agent47!")
+
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    # Now we can access `args.user_prompt`
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key = api_key)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages
+    )
+    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
+    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+    print(response.text)
+
+
 
 
 if __name__ == "__main__":
