e45abc59000203bc64e652a87e0c92289b114650
Matt-Yates <mattyatesecu@gmail.com>
2026-02-16 11:24:12 -0500

    Added file parsing capability

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action = "store_true", help="Enable verbose output")
     args = parser.parse_args()
     # Now we can access `args.user_prompt`
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]       
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key = api_key)
     response = client.models.generate_content(
         model='gemini-2.5-flash', 
-        contents=messages
-    )
-    print("Prompt tokens: " + str(response.usage_metadata.prompt_token_count))
-    print("Response tokens: " + str(response.usage_metadata.candidates_token_count))
+        contents=messages)
+    tokens = ("Prompt tokens: " + str(response.usage_metadata.prompt_token_count) + "\nResponse tokens: " + str(response.usage_metadata.candidates_token_count))
+    if args.verbose:
+        
+        print("User prompt: " + args.user_prompt)
+        print(tokens)
+
     print(response.text)
 
 
