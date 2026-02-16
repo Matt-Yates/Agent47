3d860cbbe908dbfcb9f418191499e2e11c3d9d90
Matt-Yates <mattyatesecu@gmail.com>
2026-02-16 18:56:48 -0500

    Added agent function call parameters

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,7 @@
 import os
+from google import genai
+from google.genai import types
+
 
 schema_get_files_info = types.FunctionDeclaration(
     name="get_files_info",
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from google.genai import types
 from prompts import system_prompt
 from call_function import available_functions
+from call_function import call_function
 
 
 def main():
@@ -28,21 +29,28 @@ def main():
         contents=messages,
         config=config,
         )
-
+    
     tokens = ("Prompt tokens: " + str(response.usage_metadata.prompt_token_count) + "\nResponse tokens: " + str(response.usage_metadata.candidates_token_count))
     if args.verbose:
         
         print("User prompt: " + args.user_prompt)
         print(tokens)
+
     
     if response.function_calls:
         for call in response.function_calls:
-            print(f"Calling function: {call.name}({call.args})")
-    else:
-        print(response.text)
+            function_call_result = call_function(call, args.verbose)
+            if not function_call_result.parts:
+                raise Exception("No parts in function call result")
+            if not function_call_result.parts[0].function_response:
+                raise Exception("No function response object")
+            if not function_call_result.parts[0].function_response.response:
+                raise Exception("No function result present")
 
-
-
-
+            function_results.append(function_call_result.parts[0])
+        
+            if args.verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+                
 if __name__ == "__main__":
     main()
